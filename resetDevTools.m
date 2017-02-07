8bfd41bd5f3f4d618444b670c507b36b3543d13c
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-07 16:11:24 +0100

    Minor changes in resetDevTools

--- resetDevTools.m
+++ resetDevTools.m
@@ -3,14 +3,8 @@ function resetDevTools()
     global gitConf
     global gitCmd
 
-    tmpCmd = gitCmd;
-
     clear global gitConf;
     clear global gitCmd;
 
-    if exist(tmpCmd, 'var')
-        fprintf([tmpCmd.lead, 'The development tools have been reset.', tmpCmd.success, tmpCmd.trail]);
-    else
-        fprintf('Nothing to be done.\n')
-    end
+    fprintf('The development tools have been reset.\n');
 end
