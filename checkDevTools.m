57c5a226d9a7c00e23985a9917c166583559b4a2
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-07 14:59:24 +0100

    Initial version of checkDevTools

---
+++ checkDevTools.m
@@ -0,0 +1,13 @@
+function checkDevTools()
+
+    global gitConf
+    global gitCmd
+
+    % check if the forked directory already exists
+    if ~isfield(gitConf, 'username') || ~isfield(gitConf, 'localDir')
+        initDevTools();
+    else
+        fprintf([gitCmd.lead, 'The development tools are properly configured.', gitCmd.success, gitCmd.trail]);
+    end
+
+end
