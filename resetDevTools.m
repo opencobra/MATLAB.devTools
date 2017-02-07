6b5e309ea5fcd0e5fb0ff79b6a87471e3189b371
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-07 14:45:35 +0100

    Return message for resetDevTools

--- resetDevTools.m
+++ resetDevTools.m
@@ -1,11 +1,12 @@
-function status = resetDevTools()
+function resetDevTools()
 
-  global gitConf
-  global gitCmd
+    global gitConf
+    global gitCmd
 
-  clear global gitConf;
-  clear global gitCmd;
+    tmpCmd = gitCmd;
 
-  status = 0;
+    clear global gitConf;
+    clear global gitCmd;
 
+    fprintf([tmpCmd.lead, 'The development tools have been reset.', tmpCmd.success, tmpCmd.trail]);
 end
