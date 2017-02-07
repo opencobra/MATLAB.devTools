f5e0a436908e55e298997d392fd2d80b4b4cb6b2
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-07 15:19:24 +0100

    Initial version of initDevTools

---
+++ initDevTools.m
@@ -0,0 +1,76 @@
+function initDevTools(repoURL)
+
+    global gitConf
+    global gitCmd
+
+    % check if the system is configured properly
+    checkSystem();
+
+    % main public repository
+    if nargin < 1
+        gitConf.remoteRepoURL = 'https://github.com/cobrabot/trial_wo_errors.git';%'https://github.com/opencobra/cobratoolbox.git';
+    else
+        gitConf.remoteRepoURL = repoURL;
+    end
+
+    if ~isfield(gitConf, 'username'), gitConf.username = []; end
+    if ~isfield(gitConf, 'localDir'), gitConf.localDir = []; end
+
+    % parse the remoteRepoURL
+    sepIndices = strfind(gitConf.remoteRepoURL, '/');
+    gitConf.remoteServerName = gitConf.remoteRepoURL(1:sepIndices(3));
+    gitConf.remoteRepoName = gitConf.remoteRepoURL(sepIndices(4)+1:end-4);
+    gitConf.remoteUserName = gitConf.remoteRepoURL(sepIndices(3)+1:sepIndices(4)-1);
+
+    % request the Github username
+    if isempty(gitConf.username)
+        gitConf.username = input(' -> Please enter your Github username: ', 's');
+    end
+
+    % request the local directory
+    if isempty(gitConf.localDir)
+        reply = input([' -> Please define the local path to your fork\n    example: ~/work/git\n    current: ', pwd,'\n    Enter the path (press ENTER to use the current path): '], 's');
+
+        if isempty(reply)
+            gitConf.localDir = pwd;
+        else
+            gitConf.localDir = reply;
+        end
+
+        % add a fileseparator if not included
+        if ~strcmp(gitConf.localDir(end), filesep)
+            gitConf.localDir = [gitConf.localDir, filesep];
+        end
+
+        if exist(gitConf.localDir, 'dir') ~= 7
+            reply = input(' -> The specified directory does not exist. Do you want to create it? Y/N [Y]:', 's');
+
+            % create the directory if requested
+            if isempty(reply) || strcmp(reply, 'Y')
+                system(['mkdir ', gitConf.localDir]);
+                fprintf([gitCmd.lead, 'The directory has been created.', gitCmd.success, gitCmd.trail]);
+            else
+                warning([gitCmd.lead, 'The specified directory does not exist.', gitCmd.fail]);
+            end
+        end
+    end
+
+    gitConf.forkDirName = [gitConf.leadForkDirName, gitConf.remoteRepoName];
+    gitConf.fullForkDir = [gitConf.localDir, gitConf.forkDirName];
+
+    % clone the fork
+    % cloneFork();
+
+    % proceed with configuring the fork
+    % configureFork();
+
+    % update the fork
+
+    % print the current configuration
+    fprintf([gitCmd.lead, '- Configuration --', gitCmd.trail, gitCmd.trail])
+    fprintf([gitCmd.lead, '    GitHub username:      ', gitConf.username, gitCmd.trail]);
+    fprintf([gitCmd.lead, '    Local directory :     ', gitConf.fullForkDir, gitCmd.trail])
+    fprintf([gitCmd.lead, '    Remote fork URL:      ', gitConf.forkURL, gitCmd.trail]);
+    fprintf([gitCmd.lead, '    Remote opencobra URL: ', gitConf.remoteRepoURL, gitCmd.trail]);
+
+end
