1bb6802f86be4dadfa097caab93a3f673c6d5781
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-15 14:21:44 +0100

    Initial release of openPR

---
+++ openPR.m
@@ -0,0 +1,17 @@
+function openPR(branchName)
+
+    global gitConf
+    global gitCmd
+
+    % change the directory to the local directory of the fork
+    cd(gitConf.fullForkDir);
+
+    prURL = [gitConf.remoteRepoURL(1:end-4), '/compare/develop...', gitConf.userName, ':', branchName];
+
+    [status, result] = system(['curl -s --head ', prURL, '| head -n 1']);
+
+    if status == 0 && contains(result, '200 OK')
+        fprintf([gitCmd.lead, 'You can open a pull request (PR) by clicking on \n\n\t', prURL, '?expand=1\n\n']);
+    else
+        fprintf([gitCmd.lead, 'The branch ', branchName, ' does not exist or has no commits.', gitCmd.fail, gitCmd.trail])
+    end
