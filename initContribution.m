8439e694be384da17e1d02f3749c4c116ce0df8c
Laurent Heirendt <laurent.heirendt@uni.lu>
2017-02-15 19:16:02 +0100

    Minor changes to initContribution

--- initContribution.m
+++ initContribution.m
@@ -1,4 +1,4 @@
-function initContribution()
+function initContribution(branchName)
 
     global gitConf
     global gitCmd
@@ -7,16 +7,18 @@ function initContribution()
     initDevTools();
 
     % request a name of the new feature
-    branchName = input([gitCmd.lead, ' -> Please enter a name of the feature that you want to work on (example: add-constraints): '], 's');
+    if nargin < 1
+        branchName = input([gitCmd.lead, ' -> Please enter a name of the feature that you want to work on (example: add-constraints): '], 's');
 
-    if isempty(branchName)
-        branchName = ['new-feature-', num2str(floor(floor(sum(clock))*randi(100)/100))];
+        if isempty(branchName)
+            branchName = ['new-feature-', num2str(floor(floor(sum(clock))*randi(100)/100))];
+        end
     end
 
     % checkout the branch of the feature
     checkoutBranch(branchName);
 
     % provide a success message
-    fprintf([gitCmd.lead, ' -> You may now start working on your new feature on your branch <', branchName, '>.', gitCmd.trail]);
+    fprintf([gitCmd.lead, ' -> You may now start working on your new feature <', branchName, '>.', gitCmd.trail]);
     fprintf([gitCmd.lead, ' -> You may run "deleteContribution(\''', branchName, '\''); if you want to delete your existing contribution.', gitCmd.trail]);
     fprintf([gitCmd.lead, ' -> Please run "submitContribution(\''', branchName, '\''); once you are done.', gitCmd.trail]);
