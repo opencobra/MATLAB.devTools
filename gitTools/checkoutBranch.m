14ce5ea908f94beeb9c4b18eba1aac1486f91d27
Laurent Heirendt <laurent.heirendt@uni.lu>
2017-02-16 18:02:22 +0100

    Minor changes to checkoutBranch

--- gitTools/checkoutBranch.m
+++ gitTools/checkoutBranch.m
@@ -18,23 +18,23 @@ function checkoutBranch(branchName)
     indexDevelop = strfind(resultList, 'develop');
     indexStar = strfind(resultList, '*');
 
-    if status == 0 && abs(indexDevelop-indexStar) > 10 % colors might be denoted as [32m etc.
+    if status == 0 && isempty(indexDevelop) %&& abs(indexDevelop-indexStar) > 10 % colors might be denoted as [32m etc.
         if gitConf.verbose
-            fprintf([gitCmd.lead, 'The current branch is not the develop branch.', gitCmd.fail, gitCmd.trail]);
+            fprintf([gitCmd.lead, 'The current branch is not the <develop> branch.', gitCmd.fail, gitCmd.trail]);
         end
 
         % update the fork locally
-        updateFork();
+        updateFork(true);
 
         % checkout the develop branch
         [status, result] = system('git checkout develop');
 
         if status == 0 && (contains(resultList, '* develop') || contains(result, 'Already on'))
             if gitConf.verbose
-                fprintf([gitCmd.lead, 'The current branch is develop.', gitCmd.success, gitCmd.trail]);
+                fprintf([gitCmd.lead, 'The current branch is <develop>.', gitCmd.success, gitCmd.trail]);
             end
         else
-            error([gitCmd.lead, 'An error occurred and the develop branch cannot be checked out']);
+            error([gitCmd.lead, 'An error occurred and the <develop> branch cannot be checked out']);
         end
 
         % make sure that the develop branch is up to date
@@ -42,10 +42,10 @@ function checkoutBranch(branchName)
 
         if status == 0
             if gitConf.verbose
-                fprintf([gitCmd.lead, 'The changes of the develop branch of your fork have been pulled.', gitCmd.success, gitCmd.trail]);
+                fprintf([gitCmd.lead, 'The changes of the <develop> branch of your fork have been pulled.', gitCmd.success, gitCmd.trail]);
             end
         else
-            error([gitCmd.lead, 'The changes of the develop branch could not be pulled.', gitCmd.fail, gitCmd.trail]);
+            error([gitCmd.lead, 'The changes of the <develop> branch could not be pulled.', gitCmd.fail, gitCmd.trail]);
         end
     end
 
@@ -59,7 +59,7 @@ function checkoutBranch(branchName)
 
     if status == 0
         if gitConf.verbose
-            fprintf([gitCmd.lead, 'The ', branchName, ' branch has been checked out.', gitCmd.success, gitCmd.trail]);
+            fprintf([gitCmd.lead, 'The <', branchName, '> branch has been checked out.', gitCmd.success, gitCmd.trail]);
         end
 
         % rebase if the branch already existed
@@ -72,17 +72,17 @@ function checkoutBranch(branchName)
 
                 if status == 0
                     if gitConf.verbose
-                        fprintf([gitCmd.lead, 'The ', branchName, ' branch has been rebased.', gitCmd.success, gitCmd.trail]);
+                        fprintf([gitCmd.lead, 'The <', branchName, '> branch has been rebased with <develop>.', gitCmd.success, gitCmd.trail]);
                     end
                     % push by force the rebased branch
                     [status, ~] = system(['git push origin ', branchName, '--force']);
                     if status == 0
                         if gitConf.verbose
-                            fprintf([gitCmd.lead, 'The ', branchName, ' branch has been pushed to your fork by force.', gitCmd.success, gitCmd.trail]);
+                            fprintf([gitCmd.lead, 'The <', branchName, '> branch has been pushed to your fork by force.', gitCmd.success, gitCmd.trail]);
                         end
                     end
                 else
-                    error([gitCmd.lead, 'The ', branchName, ' branch could not be rebased.', gitCmd.fail]);
+                    error([gitCmd.lead, 'The <', branchName, '> branch could not be rebased.', gitCmd.fail]);
                 end
             end
         else
@@ -91,14 +91,14 @@ function checkoutBranch(branchName)
 
             if status == 0
                 if gitConf.verbose
-                    fprintf([gitCmd.lead, 'The ', branchName, ' branch has been pushed to your fork.', gitCmd.success, gitCmd.trail]);
+                    fprintf([gitCmd.lead, 'The <', branchName, '> branch has been pushed to your fork.', gitCmd.success, gitCmd.trail]);
                 end
             else
-                error([gitCmd.lead, 'The ', branchName, ' branch could not be pushed to your fork.', gitCmd.fail, gitCmd.trail]);
+                error([gitCmd.lead, 'The <', branchName, '> branch could not be pushed to your fork.', gitCmd.fail, gitCmd.trail]);
             end
         end
     else
-        error([gitCmd.lead, 'The branch ', branchName, ' cannot be checked out.', gitCmd.fail, gitCmd.trail]);
+        error([gitCmd.lead, 'The branch <', branchName, '> cannot be checked out.', gitCmd.fail, gitCmd.trail]);
     end
 
     % change back to the current directory
