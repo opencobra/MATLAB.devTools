ba982840067c65f82eb78b39838e139ce7af9c4a
Laurent Heirendt <laurent.heirendt@uni.lu>
2017-02-16 18:07:00 +0100

    Delete local branch that is pushed

--- deleteContribution.m
+++ deleteContribution.m
@@ -9,14 +9,13 @@ function deleteContribution(branchName)
     reply = input([gitCmd.lead, 'Are you sure that you want to delete the branch <', branchName, '>? Y/N [N]: '], 's');
 
     if ~isempty(reply) && (strcmp(reply, 'y') || strcmp(reply, 'Y'))
+        % checkout the develop branch
+        checkoutBranch('develop');
 
         %check here if branch exists
         [status, result] = system(['curl -s --head ', gitConf.remoteServerName, gitConf.userName, '/', gitConf.remoteRepoName, '/tree/', branchName, '| head -n 1']);
 
         if status == 0 && contains(result, '200 OK')
-
-            % checkout the develop branch
-            checkoutBranch('develop');
 
             % delete the remote branch
             [status, ~] = system(['git push origin --delete ', branchName]);
@@ -29,11 +28,13 @@ function deleteContribution(branchName)
 
         end
 
-        [status, ~] = system(['git branch -D ', branchName]);
+        [status, result] = system(['git branch -D ', branchName]);
 
         if status == 0
             fprintf([gitCmd.lead, 'The local <', branchName, '> branch has been deleted.', gitCmd.success, gitCmd.trail]);
         else
+            result
             error([gitCmd.lead, 'The local <', branchName,'> branch could not be deleted.', gitCmd.fail, gitCmd.trail]);
         end
     end
+end
