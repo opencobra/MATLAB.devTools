c5b592c89c1b1d7663566c96c34bea1bda7a6db0
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-28 17:58:11 +0100

    First release of tests

---
+++ test/testDevTools.m
@@ -0,0 +1,18 @@
+addpath(genpath('../'))
+
+
+global gitConf
+global gitCmd
+
+% turn on the verbose mode
+gitConf.verbose = true;
+
+checkSystem();
+
+initDevTools('https://github.com/cobrabot/trial_wo_errors.git')
+
+%contribute
+
+%matlab -r "testDevTools" < testInput_1.txt
+
+%system('rm -rf fork-trial_wo_errors');
