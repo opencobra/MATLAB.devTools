eec585379ebd0bbbbe50594ecfb0adf8b0fa4d62
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-03 16:44:14 +0100

    changes to launchTests

--- test/launchTests.m
+++ test/launchTests.m
@@ -1,1 +1,21 @@
+% do not change the paths below
+addpath(genpath('/var/lib/jenkins/MOcov'));
+addpath(genpath('/var/lib/jenkins/jsonlab'));
+
+% include the root folder and all subfolders
+
+pwd
+
+addpath(genpath(pwd))
+
+
+global gitConf
+global gitCmd
+
+% turn on the verbose mode
+gitConf.verbose = true;
+
+%checkSystem();
+
+
 exit_code = 0;
