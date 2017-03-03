4f1d24a50048d7b0314589995cdff3269228b02d
syarra <sylvain.arreckx@gmail.com>
2017-03-03 15:02:14 +0100

    fix typo for username.

--- src/checkDevTools.m
+++ src/checkDevTools.m
@@ -8,7 +8,7 @@ function checkDevTools()
     global gitCmd
 
     % check if the forked directory already exists
-    if ~isfield(gitConf, 'username') || ~isfield(gitConf, 'localDir')
+    if ~isfield(gitConf, 'userName') || ~isfield(gitConf, 'localDir')
         initDevTools();
     else
         fprintf([gitCmd.lead, ' [', mfilename,'] The development tools are properly configured.', gitCmd.success, gitCmd.trail]);
