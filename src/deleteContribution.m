7b09b8c6cfef4da92397e4a60b3597971b25e6f0
laurentheirendt <laurent.heirendt@uni.lu>
2018-02-21 15:28:12 +0100

    Loop for requesting confirmation of deletion

--- src/deleteContribution.m
+++ src/deleteContribution.m
@@ -24,60 +24,64 @@ function deleteContribution(branchName)
     end
 
     if isempty(strfind(branchName, 'develop')) && isempty(strfind(branchName, 'master'))
-        reply = input([gitCmd.lead, originCall, 'Are you sure that you want to delete the feature (branch) <', branchName, '>? YES/NO [NO]: '], 's');
+        reply = '';
 
-        if ~isempty(reply) && strcmpi(reply, 'yes') % users MUST enter 'yes', not only 'y'
+        while isempty(reply) || ~strcmpi(reply, 'yes')
 
-            % checkout the develop branch
-            checkoutBranch('develop');
+            reply = input([gitCmd.lead, originCall, 'Are you sure that you want to delete the feature (branch) <', branchName, '>? YES/NO [NO]: '], 's');
 
-            % retrieve a list of all the branches
-            if ispc
-                filterColor = '';
-            else
-                filterColor =  '| tr -s "[:cntrl:]" "\n"';
-            end
+            if strcmpi(reply, 'yes') % users MUST enter 'yes', not only 'y'
+                % checkout the develop branch
+                checkoutBranch('develop');
 
-            [status_gitBranch, resultList] = system(['git branch --list ', filterColor]);
+                % retrieve a list of all the branches
+                if ispc
+                    filterColor = '';
+                else
+                    filterColor =  '| tr -s "[:cntrl:]" "\n"';
+                end
 
-            % delete the local branch
-            if status_gitBranch == 0
-                arrResult = strsplit(resultList, '\n');
-                arrResult(~cellfun(@isempty, arrResult));
+                [status_gitBranch, resultList] = system(['git branch --list ', filterColor]);
 
-                if checkBranchExistence(branchName)
-                    % delete the branch locally
-                    [status_gitBranchDelete, result_gitBranchDelete] = system(['git branch -D ', branchName]);
+                % delete the local branch
+                if status_gitBranch == 0
+                    arrResult = strsplit(resultList, '\n');
+                    arrResult(~cellfun(@isempty, arrResult));
 
-                    if status_gitBranchDelete == 0
-                        fprintf([gitCmd.lead, originCall, 'The local <', branchName, '> feature (branch) has been deleted.', gitCmd.success, gitCmd.trail]);
+                    if checkBranchExistence(branchName)
+                        % delete the branch locally
+                        [status_gitBranchDelete, result_gitBranchDelete] = system(['git branch -D ', branchName]);
+
+                        if status_gitBranchDelete == 0
+                            fprintf([gitCmd.lead, originCall, 'The local <', branchName, '> feature (branch) has been deleted.', gitCmd.success, gitCmd.trail]);
+                        else
+                            fprintf(result_gitBranchDelete);
+                            error([gitCmd.lead, ' [', mfilename,'] The local <', branchName,'> feature (branch) could not be deleted. You might have unpublished/uncommitted changes.', gitCmd.fail]);
+                        end
                     else
-                        fprintf(result_gitBranchDelete);
-                        error([gitCmd.lead, ' [', mfilename,'] The local <', branchName,'> feature (branch) could not be deleted. You might have unpublished/uncommitted changes.', gitCmd.fail]);
+                        fprintf([gitCmd.lead, originCall, 'The local <', branchName,'> feature (branch) does not exist.', gitCmd.fail, gitCmd.trail]);
                     end
                 else
-                    fprintf([gitCmd.lead, originCall, 'The local <', branchName,'> feature (branch) does not exist.', gitCmd.fail, gitCmd.trail]);
+                    error([gitCmd.lead, ' [', mfilename,'] The list of features (branches) could not be retrieved.', gitCmd.fail]);
                 end
-            else
-                error([gitCmd.lead, ' [', mfilename,'] The list of features (branches) could not be retrieved.', gitCmd.fail]);
-            end
 
-            % check if branch exists remotely
-            [status_curl, result_curl] = system(['curl -s -k --head ', gitConf.remoteServerName, gitConf.userName, '/', gitConf.remoteRepoName, '/tree/', branchName]);
+                % check if branch exists remotely
+                [status_curl, result_curl] = system(['curl -s -k --head ', gitConf.remoteServerName, gitConf.userName, '/', gitConf.remoteRepoName, '/tree/', branchName]);
 
-            % delete the remote branch
-            if status_curl == 0 && ~isempty(strfind(result_curl, '200 OK'))
+                % delete the remote branch
+                if status_curl == 0 && ~isempty(strfind(result_curl, '200 OK'))
 
-                [status_gitPush, result_gitPush] = system(['git push origin --delete ', branchName]);
+                    [status_gitPush, result_gitPush] = system(['git push origin --delete ', branchName]);
 
-                if status_gitPush == 0
-                    fprintf([gitCmd.lead, originCall, 'The remote <', branchName, '> feature (branch) has been deleted.', gitCmd.success, gitCmd.trail]);
+                    if status_gitPush == 0
+                        fprintf([gitCmd.lead, originCall, 'The remote <', branchName, '> feature (branch) has been deleted.', gitCmd.success, gitCmd.trail]);
+                    else
+                        fprintf(result_gitPush);
+                        error([gitCmd.lead, ' [', mfilename,'] The remote <', branchName,'> feature (branch) could not be deleted.', gitCmd.fail]);
+                    end
                 else
-                    fprintf(result_gitPush);
-                    error([gitCmd.lead, ' [', mfilename,'] The remote <', branchName,'> feature (branch) could not be deleted.', gitCmd.fail]);
+                    fprintf([gitCmd.lead, originCall, 'The remote <', branchName,'> feature (branch) does not exist.', gitCmd.fail, gitCmd.trail]);
                 end
-            else
-                fprintf([gitCmd.lead, originCall, 'The remote <', branchName,'> feature (branch) does not exist.', gitCmd.fail, gitCmd.trail]);
             end
         end
     else
